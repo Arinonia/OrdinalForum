2ff3454e6b1f587a3743b5310bfc99c160ed7708
tleulier <leulierthibaud@gmail.com>
2024-02-28 18:29:54 +0100

    :tada: Starting project

---
+++ src/main/java/fr/arinonia/ordinalforum/OrdinalForumApplication.java
@@ -0,0 +1,13 @@
+package fr.arinonia.ordinalforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdinalForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdinalForumApplication.class, args);
+    }
+
+}
